4a1254b509e077e9fa3705a7c6c78527418db6dc
Ajay Singh <ajaysingh8923@yahoo.com>
2025-02-10 22:58:39 +0530

    CAN_NODE2

---
+++ CAN_NODE_2/Debug/Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.d
@@ -0,0 +1,18 @@
+Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.o: \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.c \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_Int.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_2/Middleware/SEGGER/Config/Global.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_2/Middleware/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h \
+ ../Middleware/SEGGER/SEGGER/SEGGER_RTT.h
+../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_Int.h:
+../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+../Middleware/SEGGER/SEGGER/SEGGER.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_2/Middleware/SEGGER/Config/Global.h:
+../Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_2/Middleware/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+../Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h:
+../Middleware/SEGGER/SEGGER/SEGGER_RTT.h:
