51520fb6d70d52bb23f69319b093f605d688c10a
Ajay Singh <ajaysingh8923@yahoo.com>
2025-02-10 22:41:44 +0530

    CAN_Node-1

---
+++ CAN_NODE_1/Debug/Middleware/FreeRTOS/tasks.d
@@ -0,0 +1,39 @@
+Middleware/FreeRTOS/tasks.o: ../Middleware/FreeRTOS/tasks.c \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/FreeRTOSV11/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/Config/Global.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/projdefs.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/portable.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/task.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/list.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/timers.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/task.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/stack_macros.h
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/FreeRTOSV11/SEGGER_SYSVIEW_FreeRTOS.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/Config/Global.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/projdefs.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/portable.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/task.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/list.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/timers.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/task.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/FreeRTOS/include/stack_macros.h:
---
+++ CAN_NODE_1/Debug/Middleware/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+Middleware/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../Middleware/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_RTT.h:
+C:/Users/Ajay\ Singh/Desktop/RTOS_PRA/CAN_NODE_1/Middleware/SEGGER/SEGGER/SEGGER_RTT_Conf.h:
